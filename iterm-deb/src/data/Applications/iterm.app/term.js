0d0d1d02df2b9c84b7357679f5bd987d82342a2e
Sakurai07 <cool5tarXV@gmail.com>
2021-03-24 13:33:17 +0800

    added iterm + enjoy :)

---
+++ iterm-deb/src/data/Applications/iterm.app/term.js
@@ -0,0 +1,155 @@
+hterm.defaultStorage = new lib.Storage.Memory();
+window.onload = async function() {
+    await lib.init();
+    window.term = new hterm.Terminal();
+
+    // make everything invisible so as to not be embarrassing
+    term.getPrefs().set('background-color', 'transparent');
+    term.getPrefs().set('foreground-color', 'transparent');
+    term.getPrefs().set('cursor-color', 'transparent');
+
+    term.getPrefs().set('terminal-encoding', 'iso-2022');
+    term.getPrefs().set('enable-resize-status', false);
+    term.getPrefs().set('copy-on-select', false);
+    term.getPrefs().set('enable-clipboard-notice', false);
+    term.getPrefs().set('user-css-text', termCss);
+    term.getPrefs().set('screen-padding-size', 4);
+
+    term.onTerminalReady = onTerminalReady;
+    term.decorate(document.getElementById('terminal'));
+};
+
+var termCss = `
+x-screen {
+    background: transparent !important;
+    overflow: hidden !important;
+    -webkit-tap-highlight-color: transparent;
+}
+x-row {
+  text-rendering: optimizeLegibility;
+  font-variant-ligatures: normal;
+}
+`;
+
+function onTerminalReady() {
+
+// Shorthand for JS -> native IPC
+const native = new Proxy({}, {
+    get(obj, prop) {
+        return (...args) => {
+            if (args.length == 0)
+                args = null;
+            else if (args.length == 1)
+                args = args[0];
+            webkit.messageHandlers[prop].postMessage(args);
+        };
+    },
+});
+
+// Functions for native -> JS
+window.exports = {};
+
+term.io.push();
+term.reset();
+
+let oldProps = {};
+function syncProp(name, value) {
+    if (oldProps[name] !== value)
+        native.propUpdate(name, value);
+}
+let decoder = new TextDecoder();
+exports.write = (data) => {
+    term.io.writeUTF16(decoder.decode(lib.codec.stringToCodeUnitArray(data)));
+    syncProp('applicationCursor', term.keyboard.applicationCursor);
+};
+term.io.sendString = term.io.onVTKeyStroke = (data) => {
+    native.sendInput(data);
+};
+
+// hterm size updates native size
+term.io.onTerminalResize = () => native.resize();
+exports.getSize = () => [term.screenSize.width, term.screenSize.height];
+
+// selection, copying
+term.scrollPort_.screen_.contentEditable = false;
+term.blur();
+term.focus();
+exports.copy = () => term.copySelectionToClipboard();
+
+// focus
+// This listener blocks blur events that come in because the webview has lost first responder
+term.scrollPort_.screen_.addEventListener('blur', (e) => {
+    if (e.target.ownerDocument.activeElement == e.target) {
+        e.stopPropagation();
+    }
+}, {capture: true});
+term.scrollPort_.screen_.addEventListener('mousedown', (e) => {
+    // Taps while there is a selection should be left to the selection view
+    if (document.getSelection().rangeCount != 0) return;
+    native.focus();
+});
+exports.setFocused = (focus) => {
+    if (focus)
+        term.focus();
+    else
+        term.blur();
+};
+term.scrollPort_.screen_.addEventListener('focus', (e) => native.syncFocus());
+
+// scrolling
+// Disable hterm builtin touch scrolling
+term.scrollPort_.onTouch = (e) => {
+    // Convince hterm that we called preventDefault() and that it shouldn't do more handling, but don't actually call it because that would break text selection
+    Object.defineProperty(e, 'defaultPrevented', {value: true});
+};
+// Scroll to bottom wrapper
+exports.scrollToBottom = () => term.scrollEnd();
+// Set scroll position
+exports.newScrollTop = (y) => {
+    // two lines instead of one because the value you read out of scrollTop can be different from the value you write into it
+    term.scrollPort_.screen_.scrollTop = y;
+    lastScrollTop = term.scrollPort_.screen_.scrollTop;
+};
+
+// Send scroll height and position to native code
+let lastScrollHeight, lastScrollTop;
+function syncScroll() {
+    const scrollHeight = parseFloat(term.scrollPort_.scrollArea_.style.height);
+    if (scrollHeight != lastScrollHeight)
+        native.newScrollHeight(scrollHeight);
+    lastScrollHeight = scrollHeight;
+
+    const scrollTop = term.scrollPort_.screen_.scrollTop;
+    if (scrollTop != lastScrollTop)
+        native.newScrollTop(scrollTop);
+    lastScrollTop = scrollTop;
+}
+
+const realSyncScrollHeight = hterm.ScrollPort.prototype.syncScrollHeight;
+hterm.ScrollPort.prototype.syncScrollHeight = function() {
+    realSyncScrollHeight.call(this);
+    syncScroll();
+};
+term.scrollPort_.screen_.addEventListener('scroll', syncScroll);
+
+exports.updateStyle = ({foregroundColor, backgroundColor, fontFamily, fontSize}) => {
+    term.getPrefs().set('background-color', backgroundColor);
+    term.getPrefs().set('foreground-color', foregroundColor);
+    term.getPrefs().set('cursor-color', foregroundColor);
+    term.getPrefs().set('font-family', fontFamily);
+    term.getPrefs().set('font-size', fontSize);
+};
+    
+exports.getCharacterSize = () => {
+    return [term.scrollPort_.characterSize.width, term.scrollPort_.characterSize.height];
+};
+
+exports.clearScrollback = () => term.clearScrollback();
+exports.setUserGesture = () => term.accessibilityReader_.hasUserGesture = true;
+
+hterm.openUrl = (url) => native.openLink(url);
+
+native.load();
+native.syncFocus();
+
+}
